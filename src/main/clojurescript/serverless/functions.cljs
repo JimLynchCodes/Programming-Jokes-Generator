f3c510326b67f6afa4d300e9d042376baa016539
JimTheMan <mrdotjim@gmail.com>
2020-01-15 21:04:26 -0500

    not failing example

--- src/main/clojurescript/serverless/functions.cljs
+++ src/main/clojurescript/serverless/functions.cljs
@@ -1,12 +1,15 @@
 (ns serverless.functions
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljs.nodejs :as nodejs]
+  (:require [schema.core :as s]
+            [cljs.nodejs :as nodejs]
             [cljs-http.client :as http]
             [cljs.core.async :refer [put! chan <!]]))
 
 (nodejs/enable-util-print!)
 (defonce moment (nodejs/require "moment"))
 (set! js/XMLHttpRequest (nodejs/require "xhr2"))
+
+(s/set-fn-validation! true)
 
 (defn healthCheck [event ctx cb]
   (println ctx)
@@ -23,31 +26,36 @@
             :headers    {"Content-Type" "text/html"}
             :body       (str "<h1>" (.format (moment.) "LLLL") "</h1>")})))
 
-;  TODO - Add type annotations to describe what properties are 
-;  available on the "event" and "ctx" here (and ability to 
-;  ctrl +  to the implementation would be awesome!!)
-(defn jokes [event ctx cb]
-  (go (let [response (<! (http/get "https://sv443.net/jokeapi/category/Programming"))
+; Purposely incorrect schema
+(def Joke {:category s/String
+           :type     s/String
+           :joke     s/String
+           :id       s/Int
+           :foobarrr s/Int
+           :hmmmmm   s/String})
 
-            ;  TODO - Add type annotations to describe this object:
-            ; {
-            ;   "category": "Programming",
-            ;   "type": "single",
-            ;   "joke": "___",
-            ;   "id": 0
-            ;  }
-            joke  (->> response
-                       (:body)
-                       (clj->js)
-                       (.stringify js/JSON))]
+; Doesn't fail when it should
+(s/defn unpack-joke [response] :- Joke
+  (->> response
+       (:body)))
+
+(s/defn jokes [event
+               ctx
+               cb]
+
+  (go (let [response (<! (http/get "https://sv443.net/jokeapi/category/Programming"))
+            joke (unpack-joke response)]
 
         (js/console.log event)
         (js/console.log ctx)
+        (js/console.log (str "jokin' " joke))
 
         (cb nil (clj->js
                  {:statusCode 200
                   :headers    {"Content-Type" "application/json"}
-                  :body       joke})))))
+                  :body       (->> joke
+                                   (clj->js)
+                                   (.stringify js/JSON))})))))
 
 (set! (.-exports js/module) #js
                              {:healthCheck healthCheck
